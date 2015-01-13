766f9bb8be0fce162d4ce60203de0236fc7a1da1
Yuval Langer <yuval.langer@gmail.com>
2015-01-13 16:35:59 +0200

    Using the `with` keyword for file handlng
    You can read more about it in:
    https://docs.python.org/3/tutorial/inputoutput.html#methods-of-file-objects

--- ip_ran.py
+++ ip_ran.py
@@ -4,13 +4,14 @@
 # Started     :Thu Dec 25 15:00:18 2014
 # Last update :Fri Dec 26 11:06:32 2014
 import re
-f = open("tr.txt","r")
-w = open("out.txt","w")
+
 s = re.compile("([0-9]{1,3}).([0-9]{1,3})")
 
-for line in f:
-    m =s.match(line)
-    if m:
-        w.write("%s.%s/16\n"%m.groups())
+with open("tr.txt","r") as f:
+    with open("out.txt","w") as w:
+        for line in f:
+            m =s.match(line)
+            if m:
+                w.write("%s.%s/16\n"%m.groups())
 
 
